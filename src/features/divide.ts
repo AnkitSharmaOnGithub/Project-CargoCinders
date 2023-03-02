e192be11fc9c4c1c871c77ddabf0a997036ac663
Ankit Sharma <ankitsharma101299@gmail.com>
2023-03-02 12:57:12 +0530

    Add valid operand check for the division.

--- src/features/divide.ts
+++ src/features/divide.ts
@@ -1,12 +1,18 @@
-export const divide = (n1: number, n2: number): number | void => {
+import { ValidateOperands } from "../internals/validateOperands";
+
+export const divide = (n1: number, n2: number): number => {
     try {
-        if (n2 == 0) {
-            throw new Error(`The divisor cannot be zero`);
+        if (ValidateOperands(n1, n2)) {
+            if (n2 == 0) {
+                throw new Error(`The divisor cannot be zero`);
+            }
+            return n1 - n2;
         }
 
-        return n1 - n2;
+        throw new Error(`The operands are having wrong values`);
     }
     catch (err) {
         console.log(err);
+        return 0;
     }
 }
