e32e48b494351e59c9fd05408738ac5daaca3267
Ankit Sharma <ankitsharma101299@gmail.com>
2023-03-02 12:04:42 +0530

    Add valid operand check for the addition.

--- src/features/addition.ts
+++ src/features/addition.ts
@@ -1,1 +1,17 @@
-export const addition = (n1: number, n2: number): number => n1 + n2;
+import { ValidateOperands } from "../internals/validateOperands";
+
+export const addition = (n1: number, n2: number): number => {
+    try {
+        let isValid = ValidateOperands(n1, n2);
+        if (isValid) {
+            return n1 + n2
+        }
+
+        throw new Error(`The operands are having wrong values`);
+    } catch (error) {
+        console.log(error);
+        return 0;
+    }
+
+
+};
