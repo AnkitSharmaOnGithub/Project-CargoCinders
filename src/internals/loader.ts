6b9eb3b4b130890542a64c626ac1c4813df226e3
Ankit Sharma <ankitsharma101299@gmail.com>
2023-03-02 13:19:40 +0530

    Add a loader file to load and offload all features.

---
+++ src/internals/loader.ts
@@ -0,0 +1,12 @@
+import { addition } from "../features/addition";
+import { subtract } from "../features/subtract";
+import { multiply } from "../features/multiply";
+import { divide } from "../features/divide";
+import { isNull } from "../features/isNull";
+import { isNotNull } from "../features/isNotNull";
+
+
+
+export {
+    addition, subtract, multiply, divide, isNull, isNotNull
+}
