a46a41ccba4600a60abf17ecc0fbfaafa85ab443
Ankit Sharma <ankitsharma101299@gmail.com>
2023-03-02 13:37:20 +0530

    Add operations to perform for checking the functions

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,22 @@
 
 */
 
-import * as _ from './internals/loader';
+import * as _ from "./internals/loader";
 
 const n1: number = 10;
 const n2: number = 5;
 const emptyVal = null;
+
+// Check for isNull & isNotNull
+const operations: Array<{ 'name': string, operation: any }> = [
+    { "name": "Addition", "operation": _.addition(n1, n2) },
+    { "name": "Subtraction", "operation": _.subtract(n1, n2) },
+    { "name": "Multiplication", "operation": _.multiply(n1, n2) },
+    { "name": "Division", "operation": _.divide(n1, n2) },
+    { "name": "Check isNull", "operation": _.isNull(n1) },
+    { "name": "Check isNotNull", "operation": _.isNotNull(n2) },
+];
+
+for (const op of operations) {
+    console.log(op);
+}
