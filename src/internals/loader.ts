ed24ead50fef42aed313c47eb448549deaab07a6
Ankit Sharma <ankitsharma101299@gmail.com>
2023-03-03 07:42:19 +0530

    Loaded the newly created functions, to the Loader :)

--- src/internals/loader.ts
+++ src/internals/loader.ts
@@ -1,12 +1,26 @@
-import { addition } from "../features/addition";
-import { subtract } from "../features/subtract";
-import { multiply } from "../features/multiply";
-import { divide } from "../features/divide";
-import { isNull } from "../features/isNull";
-import { isNotNull } from "../features/isNotNull";
+// Integer features
 
+import { addition } from "../features/integers/addition";
+import { subtract } from "../features/integers/subtract";
+import { multiply } from "../features/integers/multiply";
+import { divide } from "../features/integers/divide";
+import { isNull } from "../features/integers/isNull";
+import { isNotNull } from "../features/integers/isNotNull";
+import { max } from "../features/integers/max";
+import { min } from "../features/integers/min";
+import { ceil } from "../features/integers/ceil";
+import { floor } from "../features/integers/floor";
+import { randomNumber } from "../features/integers/randomNumber";
+import { roof } from "../features/integers/roof";
+import { round } from "../features/integers/round";
 
+// String features
+import { camelCase } from "../features/strings/camelCase";
+import { lowerCase } from "../features/strings/lowerCase";
+import { upperCase } from "../features/strings/upperCase";
+import { upperFirst } from "../features/strings/upperFirst";
 
 export {
-    addition, subtract, multiply, divide, isNull, isNotNull
+    addition, subtract, multiply, divide, isNull, isNotNull, max, min, ceil, floor, randomNumber, roof, round,
+    camelCase, lowerCase, upperCase, upperFirst
 }
