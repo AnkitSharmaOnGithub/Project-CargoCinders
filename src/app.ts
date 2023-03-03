67ac7fd8d2951d20f56e92cf9561c23ce2eab7e4
Ankit Sharma <ankitsharma101299@gmail.com>
2023-03-03 08:08:22 +0530

    Add the array functions, to the playground.

--- src/app.ts
+++ src/app.ts
@@ -15,18 +15,23 @@ const str: string = "hello World";
 // Check for isNull & isNotNull
 const operations: Array<{ 'name': string, operation: any }> = [
     // Integers
-    { "name": "Addition", "operation": _.addition(n1, n2) },
-    { "name": "Subtraction", "operation": _.subtract(n1, n2) },
-    { "name": "Multiplication", "operation": _.multiply(n1, n2) },
-    { "name": "Division", "operation": _.divide(n1, n2) },
-    { "name": "Check isNull", "operation": _.isNull(n1) },
-    { "name": "Check isNotNull", "operation": _.isNotNull(n2) },
+    // { "name": "Addition", "operation": _.addition(n1, n2) },
+    // { "name": "Subtraction", "operation": _.subtract(n1, n2) },
+    // { "name": "Multiplication", "operation": _.multiply(n1, n2) },
+    // { "name": "Division", "operation": _.divide(n1, n2) },
+    // { "name": "Check isNull", "operation": _.isNull(n1) },
+    // { "name": "Check isNotNull", "operation": _.isNotNull(n2) },
 
     // Strings
-    { "name": "Lowercase", "operation": _.lowerCase(str) },
-    { "name": "Uppercase", "operation": _.upperCase(str) },
-    { "name": "Upperfirst", "operation": _.upperFirst(str) },
-    // { "name": "Camelcase", "operation": _.camelCase(str) },
+    // { "name": "Lowercase", "operation": _.lowerCase(str) },
+    // { "name": "Uppercase", "operation": _.upperCase(str) },
+    // { "name": "Upperfirst", "operation": _.upperFirst(str) },
+    // // { "name": "Camelcase", "operation": _.camelCase(str) },
+
+    // Arrays
+    { "name": "CastToArray", "operation": _.castToArray(1, 2, 3) },
+    { "name": "CastToArray", "operation": _.castToArray() },
+
 ];
 
 for (const op of operations) {
