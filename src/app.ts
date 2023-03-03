7d5beac4ce50720dd056d384fe9bb3ba983d7b84
Ankit Sharma <ankitsharma101299@gmail.com>
2023-03-03 07:43:31 +0530

    Add the strings functions, to the playground.

--- src/app.ts
+++ src/app.ts
@@ -9,16 +9,24 @@ import * as _ from "./internals/loader";
 
 const n1: number = 10;
 const n2: number = 5;
-const emptyVal = null;
+const emptyVal: null = null;
+const str: string = "hello World";
 
 // Check for isNull & isNotNull
 const operations: Array<{ 'name': string, operation: any }> = [
+    // Integers
     { "name": "Addition", "operation": _.addition(n1, n2) },
     { "name": "Subtraction", "operation": _.subtract(n1, n2) },
     { "name": "Multiplication", "operation": _.multiply(n1, n2) },
     { "name": "Division", "operation": _.divide(n1, n2) },
     { "name": "Check isNull", "operation": _.isNull(n1) },
     { "name": "Check isNotNull", "operation": _.isNotNull(n2) },
+
+    // Strings
+    { "name": "Lowercase", "operation": _.lowerCase(str) },
+    { "name": "Uppercase", "operation": _.upperCase(str) },
+    { "name": "Upperfirst", "operation": _.upperFirst(str) },
+    // { "name": "Camelcase", "operation": _.camelCase(str) },
 ];
 
 for (const op of operations) {
