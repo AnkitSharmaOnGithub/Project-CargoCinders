26cf8b6ffc6c69e95465263562c0ebbc3f86fd92
Ankit Sharma <ankitsharma101299@gmail.com>
2023-03-03 07:39:08 +0530

    Add the camelCase feature.

---
+++ src/features/strings/camelCase.ts
@@ -0,0 +1,10 @@
+export const camelCase = (str: string): string => {
+    const camelCasedStr: string = "";
+
+    // Loop through the string
+
+    // Find the index for the occurence of " "
+
+    // Remove space and capatilize the words following the space
+    return camelCasedStr;
+}
