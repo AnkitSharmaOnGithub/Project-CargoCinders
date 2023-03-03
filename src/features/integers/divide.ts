34f611f71872ccb7f4595900d5071b8da48f9d9c
Ankit Sharma <ankitsharma101299@gmail.com>
2023-03-03 07:23:48 +0530

    Add the files to the new location.

---
+++ src/features/integers/divide.ts
@@ -0,0 +1,18 @@
+import { ValidateOperands } from "../internals/validateOperands";
+
+export const divide = (n1: number, n2: number): number => {
+    try {
+        if (ValidateOperands(n1, n2)) {
+            if (n2 == 0) {
+                throw new Error(`The divisor cannot be zero`);
+            }
+            return n1 / n2;
+        }
+
+        throw new Error(`The operands are having wrong values`);
+    }
+    catch (err) {
+        console.log(err);
+        return 0;
+    }
+}
---
+++ src/features/integers/multiply.ts
@@ -0,0 +1,15 @@
+import { ValidateOperands } from "../internals/validateOperands";
+
+export const multiply = (n1: number, n2: number): number => {
+    try {
+        if (ValidateOperands(n1, n2)) {
+            return n1 * n2;
+        }
+
+        throw new Error(`The operands are having wrong values`);
+    } catch (error) {
+        console.log(error);
+        return 0;
+    }
+
+};
