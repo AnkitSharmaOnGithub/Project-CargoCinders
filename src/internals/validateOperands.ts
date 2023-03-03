411fcf55c1b0ed007f39d8bc3379fbdf5f2f2f0e
Ankit Sharma <ankitsharma101299@gmail.com>
2023-03-03 07:29:17 +0530

    Fix the import path.

--- src/internals/validateOperands.ts
+++ src/internals/validateOperands.ts
@@ -1,4 +1,4 @@
-import { isNotNull } from "../features/isNotNull"
+import { isNotNull } from "../features/integers/isNotNull"
 
 export const ValidateOperands = (n1: any, n2: any): boolean => {
     return (isNotNull(n1) && isNotNull(n2));
