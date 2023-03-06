bb86e955785394e840016253709dc9c2ab60010c
Ankit Sharma <ankitsharma101299@gmail.com>
2023-03-06 18:57:41 +0530

    Correct variable names

--- src/features/arrays/chunkArray.ts
+++ src/features/arrays/chunkArray.ts
@@ -1,15 +1,15 @@
-export const chunkArray = <T>(nums: T[], size: number): (T[] | []) => {
+export const chunkArray = <T>(arr: T[], size: number): (T[] | []) => {
     try {
-        // If array size is less than 0. Throw error
         size = Math.max(size, 0);
-        const length = nums.length;
+        const length = arr.length;
+        // If array size is less than 0. Throw error
         // If size is less than 0. Throw error
         if (size < 1 || !length) {
             throw new Error(`Size and length cannot be less than 1`);
         }
 
         const rand = Math.random() * length;
-        return nums.slice(rand, size);
+        return arr.slice(rand, size);
     } catch (error) {
         console.log(error);
         return [];
